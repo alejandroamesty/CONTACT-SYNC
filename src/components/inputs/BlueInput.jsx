ebaca9342f24478bacb36b0c8353eac6217fb88e
alejandroamesty <alejandro.30337890@uru.edu>
2024-05-24 00:17:31 -0400

    feat(components-inputs): añadir nuevas propiedades

--- src/components/inputs/BlueInput.jsx
+++ src/components/inputs/BlueInput.jsx
@@ -1,20 +1,33 @@
 import React from "react";
-import { TextInput, View, StyleSheet } from "react-native";
+import { TextInput, View, StyleSheet, Image } from "react-native";
 
-const BlueInput = ({ placeholder, style }) => {
+const BlueInput = ({
+    placeholder,
+    style,
+    image,
+    width = 350,
+    height = 55,
+    fontSize = 15,
+}) => {
     return (
-        <View style={[styles.container, style]}>
+        <View style={[styles.container, { width, height }, style]}>
             <TextInput
-                style={styles.input}
+                style={[
+                    styles.input,
+                    image && styles.inputWithImage,
+                    { fontSize },
+                ]}
                 placeholder={placeholder}
                 placeholderTextColor="rgba(255, 255, 255, 0.5)"
             />
+            {image && <Image source={image} style={styles.image} />}
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
+        position: "relative",
         width: 350,
         height: 55,
     },
@@ -26,6 +39,19 @@ const styles = StyleSheet.create({
         borderRadius: 100,
         paddingHorizontal: 20,
         color: "white",
+        height: "100%",
+        paddingRight: 55,
+    },
+    inputWithImage: {
+        paddingRight: 55,
+    },
+    image: {
+        position: "absolute",
+        width: 23,
+        height: 23,
+        right: 20,
+        top: (55 - 23) / 2,
+        zIndex: 1,
     },
 });
 
