96afb07b730a484478f87731644dd6745d25cc3c
alejandroamesty <alejandro.30337890@uru.edu>
2024-05-27 15:13:03 -0400

    refactor(components-buttons): incluir width como prop

--- src/components/buttons/SaveButton.jsx
+++ src/components/buttons/SaveButton.jsx
@@ -1,39 +1,43 @@
 import React from "react";
 import { TouchableOpacity, Text, StyleSheet } from "react-native";
 
-const SaveButton = ({ onPress, title, disabled, style }) => {
-  return (
-    <TouchableOpacity
-      style={[styles.container, style, disabled && styles.disabled]}
-      onPress={onPress}
-      disabled={disabled}
-    >
-      <Text style={styles.text}>{title}</Text>
-    </TouchableOpacity>
-  );
+const SaveButton = ({ onPress, title, disabled, style, width = 350 }) => {
+    return (
+        <TouchableOpacity
+            style={[
+                styles.container,
+                { width },
+                style,
+                disabled && styles.disabled,
+            ]}
+            onPress={onPress}
+            disabled={disabled}
+        >
+            <Text style={styles.text}>{title}</Text>
+        </TouchableOpacity>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    position: "absolute",
-    width: 350,
-    height: 55,
-    backgroundColor: "#FFFFFF",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 100,
-  },
-  disabled: {
-    opacity: 0.5,
-  },
-  text: {
-    fontFamily: "BROmnySemiBold",
-    fontWeight: "600",
-    fontSize: 18,
-    lineHeight: 24,
-    textAlign: "center",
-    color: "#030B38",
-  },
+    container: {
+        position: "absolute",
+        height: 55,
+        backgroundColor: "#FFFFFF",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 100,
+    },
+    disabled: {
+        opacity: 0.5,
+    },
+    text: {
+        fontFamily: "BROmnySemiBold",
+        fontWeight: "600",
+        fontSize: 18,
+        lineHeight: 24,
+        textAlign: "center",
+        color: "#030B38",
+    },
 });
 
 export default SaveButton;
