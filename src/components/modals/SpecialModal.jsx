e6c49fe2846c6c7adceed8734ea14c6621c2c311
alejandroamesty <alejandro.30337890@uru.edu>
2024-05-27 15:07:25 -0400

    feat(components-modals): crear nuevos modales

---
+++ src/components/modals/SpecialModal.jsx
@@ -0,0 +1,141 @@
+import React from "react";
+import {
+    StyleSheet,
+    View,
+    Text,
+    Modal,
+    TouchableOpacity,
+    Image,
+} from "react-native";
+
+const SpecialModal = ({
+    visible,
+    closeModal,
+    title,
+    cancelButtonName,
+    doneButtonName,
+    cancelButtonAction,
+    doneButtonAction,
+    cancelButtonColor,
+    doneButtonColor,
+    modalContent,
+}) => {
+    console.log("modalContent:", modalContent);
+
+    return (
+        <Modal
+            animationType="slide"
+            transparent={true}
+            visible={visible}
+            onRequestClose={closeModal}
+        >
+            <View style={styles.modalContainer}>
+                <View style={styles.modalContent}>
+                    <Image
+                        source={require("../../../assets/images/DeleteModal.png")}
+                        style={styles.deleteIcon}
+                    />
+                    <Text style={styles.modalTextTitle}>{title}</Text>
+                    <Text style={styles.modalText}>
+                        Are you sure you want to delete your whole account?
+                        You'll lose everything. Your contacts and groups will be
+                        gone forever.
+                    </Text>
+                    <Text style={styles.modalText}>
+                        If you're sure, confirm by logging in below.
+                    </Text>
+                    <View style={styles.additionalContent}>
+                        <View>
+                            {modalContent}
+                        </View>
+                    </View>
+                    <View style={styles.buttonContainer}>
+                        <TouchableOpacity onPress={cancelButtonAction}>
+                            <Text
+                                style={[
+                                    styles.buttonLeft,
+                                    { color: cancelButtonColor },
+                                ]}
+                            >
+                                {cancelButtonName}
+                            </Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={doneButtonAction}>
+                            <Text
+                                style={[
+                                    styles.buttonRight,
+                                    { color: doneButtonColor },
+                                ]}
+                            >
+                                {doneButtonName}
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+        </Modal>
+    );
+};
+
+export default SpecialModal;
+
+const styles = StyleSheet.create({
+    modalContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+    },
+    modalContent: {
+        position: "absolute",
+        bottom: 0,
+        width: "100%",
+        backgroundColor: "#fff",
+        padding: 20,
+        borderTopLeftRadius: 40,
+        borderTopRightRadius: 40,
+    },
+    deleteIcon: {
+        width: 100,
+        height: 100,
+        alignSelf: "center",
+    },
+    modalTextTitle: {
+        fontFamily: "BR Omny",
+        fontWeight: "600",
+        fontSize: 25,
+        marginBottom: 10,
+        textAlign: "center",
+    },
+    modalText: {
+        fontFamily: "BR Omny",
+        width: 285,
+        fontSize: 18,
+        marginBottom: 10,
+        textAlign: "center",
+        alignSelf: "center",
+    },
+    buttonContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        width: "100%",
+        marginTop: 20,
+        marginBottom: 215,
+        paddingHorizontal: 10,
+    },
+    buttonLeft: {
+        fontFamily: "BR Omny",
+        fontWeight: "600",
+        fontSize: 16,
+        color: "#7D7D7D",
+    },
+    buttonRight: {
+        fontFamily: "BR Omny",
+        fontWeight: "600",
+        fontSize: 16,
+        color: "#F74040",
+    },
+    additionalContent: {
+        marginTop: 20,
+    },
+});
