7f4c6603246cf74f88bbc952993c014113dfb5d4
alejandroamesty <alejandro.30337890@uru.edu>
2024-05-29 21:40:07 -0400

    fix(components-inputs): añadir fuente de texto

--- src/components/inputs/GrayInput.jsx
+++ src/components/inputs/GrayInput.jsx
@@ -31,6 +31,7 @@ const styles = StyleSheet.create({
 		flex: 1,
 		backgroundColor: "#EDEEF0",
 		paddingHorizontal: 20,
+		fontFamily: "BROmnyRegular",
 		color: "black",
 		height: "100%",
 		paddingRight: 16,
